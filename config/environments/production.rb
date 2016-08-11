32f6dc8ac6899bf155af91ef9af700c945124725
Takahiro Yoshioka <hammpilot@gmail.com>
2016-08-12 01:01:39 +0900

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.raise_delivery_erros = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'frozen-garden-41244.herokuapp.com'
+  ActionMailer::Base.smtp_settings = {
+    :address                => 'smtp.sendgrid.net',
+    :port                   => '587',
+    :authentication         => :plain,
+    :user_name              => ENV['Shantti_Y'],
+    :password               => ENV['d1am0n6s'],
+    :domain                 => 'heroku.com',
+    :enable_starttls_auto   => true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
